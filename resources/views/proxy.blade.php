c7834c659f1e4dce87c5546ac5d52a6ca93e8c4d
JangJaeWon <superjang11@gmail.com>
2018-04-06 17:10:18 +0900

    Update proxy.blade.php

--- resources/views/proxy.blade.php
+++ resources/views/proxy.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <div class="supreme" style="width:100%;height:100%;">
-    <?php print file_get_contents('http://www.supremenewyork.com/'); ?>
+    <?php print file_get_contents('https://www.supremenewyork.com/'); ?>
 </div>
 @endsection
