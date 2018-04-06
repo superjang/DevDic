adae248b88b2e44017a675a8e1aaa2347ad300f8
JangJaeWon <superjang11@gmail.com>
2018-04-06 17:16:40 +0900

    Update proxy.blade.php

--- resources/views/proxy.blade.php
+++ resources/views/proxy.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <div class="supreme" style="width:100%;height:100%;">
-    <?php print file_get_contents('https://www.supremenewyork.com/'); ?>
+    <?php
+    $context = stream_context_create(
+        array(
+            "http" => array(
+                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+            )
+        )
+    );
+    print file_get_contents('https://www.supremenewyork.com/', false, $context); 
+    ?>
 </div>
 @endsection
